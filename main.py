8c7525b81e71d59d72496ac8b85f6e4ad2829589
Kate Ramizovskaya <kr978407@gmail.com>
2024-11-12 10:59:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,26 @@
-import random
+import random #подключение библиотеки рандом
 
-nums=[random.randint(-50, 50) for _ in range(25)]
+nums=[random.randint(-50, 50) for _ in range(25)] #инициализация списка значений от -50 до 50
 
 pos_num=0
 neg_num=0
 nul_num=0
-
+#инициалзация переменных для подсчитывания кол-ва совпадений
 for num in nums:
-    if num>0:
+    if num>0:#увеличение счетчика положительных
         pos_num+=1
-    if num<0:
+    if num<0:#увеличение счетчика отрицательных
         neg_num+=1
-    if num==1:
+    if num==0:#подсчет кол-ва 0
         nul_num+=1
 
-kolv_num=len(nums)
-pos_proc=(pos_num/kolv_num)*100
-neg_proc=(neg_num/kolv_num)*100
-nul_proc=(nul_num/kolv_num)*100
+kolv_num=len(nums)#количество цифр через длинну списка
+pos_proc=(pos_num/kolv_num)*100#подсчет процента положительных значений
+neg_proc=(neg_num/kolv_num)*100#подсчет процента отрицательных значений
+nul_proc=(nul_num/kolv_num)*100#подсчет процента нулей
 
-max_num=max(nums)
-min_num=min(nums)
+max_num=max(nums)#инициализация переменной с максимальным значением списка
+min_num=min(nums)#инициалхиация переменной с минимальным значением списка
 
 print(f"Список чисел: \n{nums}")
 print("\nРезультаты")
@@ -29,3 +29,4 @@ print(f"Кол-во отрицательных: {neg_num}\nПроцент отр
 print(f"Кол-во нулей: {nul_num}\nПроцент нулей: {nul_proc}%")
 print(f"Максимальное значение: {max_num}")
 print(f"Минимальное значение: {min_num}")
+#вывод итоговых значений переменных
